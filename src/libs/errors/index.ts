216befc361ad2cc6165909e79a5a4edf6259c4dc
Susovan Dev <susovandas.dev@gmail.com>
2026-01-08 15:26:44 +0530

    feat(utils): create separate error classes for centralized error handling

---
+++ src/libs/errors/index.ts
@@ -0,0 +1,21 @@
+import { HttpError } from './apiError.js';
+import { BadRequestError } from './badRequestError.js';
+import { UnauthorizedError } from './unAuthorizedError.js';
+import { ForbiddenError } from './forbiddenError.js';
+import { NotFoundError } from './notFoundError.js';
+import { ConflictError } from './conflictError.js';
+import { TooManyRequestsError } from './tooManyRequestsError.js';
+import { InternalServerError } from './InternalServerError.js';
+import { ServiceUnavailableError } from './ServiceUnavailableError.js';
+
+export {
+  HttpError,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
+  TooManyRequestsError,
+  InternalServerError,
+  ServiceUnavailableError,
+};
